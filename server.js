05a1a8a3f71fbdbdcc59d0335439aac4a9c4427a
Greg Garnhart <gegarnhart@email.wm.edu>
2019-03-14 11:07:29 -0400

    Added the badnwidth package, got an api key, secret, etc, and created a gitignore file so those remain private.

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var bodyParser = require("body-parser");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+var Bandwidth = require("node-bandwidth");
+var credentials = require("./credentials");
+console.log(credentials.apiSecret);
+var client = new Bandwidth({
+    userID : credentials.userID,
+    apiToken = credentials.apiToken,
+    apiSecret : credentials.apiSecret
+});
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -15,6 +23,11 @@ app.get("/messages", (req, res) => {
 // this is used so all the users stay in sync
 io.on("connect", socket => {
   console.log("A USER CONNECTED GUYS");
+});
+
+// Hey hey they're tyring to send something
+app.post("/messages", async (req, res) => {
+  console.log(req.body);
 });
 
 var server = http.listen(3000, () => {
